8d3f10b5a7f4d7b6539035e977227a4f6feb715a
Rai Alcides <railayli2@hotmail.com>
2022-10-27 13:07:55 -0300

    chamar um digimon por nome

--- exercicios/raiani-aluna/projeto-casa/rai-alcides/index.js
+++ exercicios/raiani-aluna/projeto-casa/rai-alcides/index.js
@@ -3,20 +3,47 @@ let chamarDigimon = async () => {
         const response = await fetch('https://digimon-api.vercel.app/api/digimon')
         const jsonDigimons = await response.json()
         console.log(jsonDigimons)
-        let body = document.querySelector('body')
         let ul = document.querySelector('ul')
         jsonDigimons.map((digimon)=>{
            
             let li = document.createElement('li')
+            
             li.innerHTML = digimon.name
-            body.append(ul)
+            // li.addEventListener('click', buscarDigimonPorNome(li.innerText))
+            // li.onclick = buscarDigimonPorNome(li.innerText)
+            li.setAttribute("onclick", "buscarDigimonPorNome(this.innerText)")
             ul.append(li)
         })
 
     } catch (error) {
-        console.error('capiturei um erro:', err) 
+        console.error('capiturei um erro:', error) 
     }
 
 }
 
 chamarDigimon()
+
+
+let criarCard =  (digimon) => {
+return `
+    <img src="${digimon.img}" alt="">
+    <h4>${digimon.name}</h4>
+    <p>${digimon.level}</p>  
+`
+}
+
+let buscarDigimonPorNome = async (name) => {
+    const card = document.getElementById('card')
+    try {
+       const response = await fetch(`https://digimon-api.vercel.app/api/digimon/name/${name}`)
+       const digimon = await response.json() 
+       console.log(digimon[0])
+       card.innerHTML =  criarCard(digimon[0]) 
+    } catch (error) {
+        console.error('capiturei um erro:', error)  
+    }
+}
+
+
+
+
