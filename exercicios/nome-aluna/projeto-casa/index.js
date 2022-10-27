b580ff4dbb096d412c955fa475d0ecfdc1db718d
Grazielle Moura <mouragrazielle@gmail.com>
2022-10-27 15:21:53 -0300

    Nomes Digimon usando API e Fetch

---
+++ exercicios/nome-aluna/projeto-casa/index.js
@@ -0,0 +1,22 @@
+let container = document.getElementById('digi')
+let dataList = document.getElementById ('nome')
+let imagemDigi = document.getElementById ('img')
+  
+let getDigimon = async () => {
+  try{
+    let response = await fetch('https://digimon-api.vercel.app/api/digimon')
+    let digimons = await response.json()
+    return digimons
+  }
+  catch(err) {
+    console.error("Capturei um erro: ", err)
+  }
+}
+
+getDigimon().then(digimons => {
+    digimons.forEach(digmon => {
+        let itemDaLista = document.createElement('option');
+        itemDaLista.innerText = digmon.name;
+        dataList.appendChild(itemDaLista)
+    });
+})
