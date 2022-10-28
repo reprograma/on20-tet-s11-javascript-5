ca0f75c9822481bb95008e0876b56bedb4118d39
Grazielle Moura <mouragrazielle@gmail.com>
2022-10-28 17:18:10 -0300

    Melhorando funcionalidades da página

--- exercicios/nome-aluna/projeto-casa/index.js
+++ exercicios/nome-aluna/projeto-casa/index.js
@@ -1,6 +1,7 @@
-let container = document.getElementById('digi')
-let dataList = document.getElementById ('nome')
-  
+let container = document.getElementById('digiDiv')
+let dataList = document.getElementById ('lista')
+
+
 let getDigimon = async () => {
   try{
     let response = await fetch('https://digimon-api.vercel.app/api/digimon')
@@ -13,11 +14,39 @@ let getDigimon = async () => {
 }
 
 getDigimon().then(digimons => {
-    digimons.forEach(digmon => {
+    digimons.forEach(digimon => {
         let itemDaLista = document.createElement('option');
-        itemDaLista.innerText = digmon.name;
+        itemDaLista.innerText = digimon.name;
         dataList.appendChild(itemDaLista)
     });
 })
+
+function pegaclick(event) {
+    event.preventDefault()
+
+    //pega texto digitado
+    let digitado = document.getElementById('input-escolhido').value
+
+    //percorre lista
+    getDigimon().then(digimons => {
+        digimons.forEach(digimon => {  
+
+            //compara texto digitado com item do loop
+            if (digitado== digimon.name) {
+                               
+                //exibe o que precisa (detalehs do digimon)
+                let imagem = document.getElementById ('trocardigi');
+                imagem.src = digimon.img;
+                container.appendChild(imagem)
+
+                digitado.value = ''
+            }
+        });
+    })
+}
+
+//pega botao e fica esperando o click
+document.getElementById('btn-escolhido').addEventListener('click', pegaclick)
+  
 
 
