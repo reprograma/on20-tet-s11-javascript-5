aac4669899d7afd6f8c937a50ced690972ea2a05
Pati Rocha <patirocha.dev@gmail.com>
2022-10-28 13:00:50 -0300

    update refactor event function

--- exercicios/pati-rocha/projeto-casa/index.js
+++ exercicios/pati-rocha/projeto-casa/index.js
@@ -1,3 +1,8 @@
+// TODO:
+// - COLOCAR O NOME DE TODOS DIGIMONS NA TELA ok
+// - CRIAR FUNÇÃO PARA MOSTRAR CARDS  ok
+// - ADICIONAR EVENTO NO BOTÃO ok
+
 const secButtons = document.getElementById('sec-buttons');
 const secCards = document.getElementById('sec-cards')
 const main = document.querySelector('main')
@@ -7,36 +12,31 @@ main.insertAdjacentHTML('beforebegin', '<h1>Escolha seu Digimon... </h1>');
 // beforeend - Dentro do elemento, após seu último filho (childNode) 
 // afterend - Após o elemento.
 
-function createCards(digimon) {
+function createCards(elemento) {
     return `
         <div class="card">
-        <img class="img-card"src= ${digimon.img} alt="imagem do digimon">
-        <h2 class= "title">${digimon.name}</h2>
-        <h3 class= "subtitle">${digimon.level}</h3>
+        <img class="img-card"src= ${elemento.img} alt="imagem do digimon">
+        <h2 class= "title">${elemento.name}</h2>
+        <h3 class= "subtitle">${elemento.level}</h3>
         </div> `
-}
-async function showCards(name) {
-    try {
-        const response = await fetch(`https://digimon-api.vercel.app/api/digimon`)
-        const dado = await response.json()
-        dado.forEach(digimon => {
-            if (name === digimon.name) {
-                secCards.innerHTML = createCards(digimon)
-            }})
-    } catch (err) {
-        console.error('Capturei um erro: ', err);
-    }
 }
 async function getDigimons() {
     try {
         const response = await fetch(`https://digimon-api.vercel.app/api/digimon`)
         const dado = await response.json()
-        dado.forEach(digimon => {
-            const button = document.createElement('button')
-            button.innerText = digimon.name
-            secButtons.appendChild(button)
-            button.addEventListener('click', () => showCards(digimon.name))
+        dado.map((digimon) => {
+            const button = document.createElement('button');
+            button.innerText = digimon.name;
+            secButtons.appendChild(button);
 
+            button.addEventListener('click', () => {
+                dado.map((digimon) => {
+                    if (button.innerText === digimon.name) {
+                        secCards.innerHTML = createCards(digimon);
+                    }
+                });
+            });
+            //button.addEventListener('click', () => showCards(digimon.name))--->FAZ PARTE DA 1ª SOLUÇÃO
         });
     }
     catch (err) {
@@ -45,6 +45,18 @@ async function getDigimons() {
 }
 getDigimons()
 
-
+//1ª SOLUÇÃO:
+// async function showCards(digimonName) {
+//     try {
+//         const response = await fetch(`https://digimon-api.vercel.app/api/digimon`)
+//         const dado = await response.json()
+//         dado.forEach(item => {
+//             if (digimonName === item.name) {
+//                 secCards.innerHTML = createCards(item)
+//             }})
+//     } catch (err) {
+//         console.error('Capturei um erro: ', err);
+//     }
+// }
 
 
